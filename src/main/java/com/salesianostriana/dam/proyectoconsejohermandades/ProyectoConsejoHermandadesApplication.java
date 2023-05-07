c2a9d179fa95b1457571587814836d8917d543e0
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-05-07 16:49:11 +0200

    first commit

---
+++ src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/ProyectoConsejoHermandadesApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoconsejohermandades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConsejoHermandadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConsejoHermandadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/proyectoconsejohermandades/ProyectoConsejoHermandadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoconsejohermandades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoConsejoHermandadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
