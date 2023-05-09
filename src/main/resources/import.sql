657f25f627f8cecddd27dbc198cd66224c0cb753
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-05-09 23:39:46 +0200

    iniciar sesion practicamente terminado

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO Sector (id, localizacion, precio) VALUES (1, 'Campana', 299);
+INSERT INTO Sector (id, localizacion, precio) VALUES (2, 'Sierpes', 249);
+INSERT INTO Sector (id, localizacion, precio) VALUES (3, 'Plaza de San Francisco', 1099);
+INSERT INTO Sector (id, localizacion, precio) VALUES (4, 'Avenida', 199);
+INSERT INTO Sector (id, localizacion, precio) VALUES (5, 'Catedral', 149);
+INSERT INTO Sector (id, localizacion, precio) VALUES (6, 'Plaza Virgen de los Reyes', 289);
+
+ALTER SEQUENCE hibernate_sequence RESTART WITH 1000;
