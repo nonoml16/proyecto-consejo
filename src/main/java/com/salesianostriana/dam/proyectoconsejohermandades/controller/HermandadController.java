820fb005faf35675b9538c9f7db7b246e11c0964
Antonio Martinez Lopez <nonomartinezlopez2004@gmail.com>
2023-05-17 14:26:31 +0200

    funcionalidad añadida correctamente

--- src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/controller/HermandadController.java
+++ src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/controller/HermandadController.java
@@ -7,7 +7,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.proyectoconsejohermandades.model.Hermandad;
@@ -28,7 +30,20 @@ public class HermandadController {
 		return "admin/hermandad/list-hermandad";
 	}
 	
+	@GetMapping("/nuevo")
+	public String nuevoHermandad(@AuthenticationPrincipal Propietario propietario, Model model) {
+		model.addAttribute("propietario", propietario);
+		model.addAttribute("hermandad", new Hermandad());
+		return "admin/hermandad/form-hermandad";
+	}
 	
+	@PostMapping("/nuevo/submit")
+	public String submitNuevoSector(@ModelAttribute("sector") Hermandad hermandad, Model model) {
+		
+		hermandadService.save(hermandad);
+		
+		return "redirect:/admin/hermandad/";
+	}
 	
 	@GetMapping("/editar/{id}")
 	public String editarHermandad(@AuthenticationPrincipal Propietario propietario, @PathVariable("id") Long id, Model model) {
