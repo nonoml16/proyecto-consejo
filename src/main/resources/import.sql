6b3b307ac835463a0dad62db199067b5669b4c56
Antonio Martinez Lopez <nonomartinezlopez2004@gmail.com>
2023-05-18 08:39:17 +0200

    minor error fixed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -62,8 +62,8 @@ INSERT INTO Hermandad (id, nombre, dia) VALUES (58, 'El Santo Entierro', 'Sábad
 INSERT INTO Hermandad (id, nombre, dia) VALUES (59, 'La Soledad', 'Sábado Santo');
 INSERT INTO Hermandad (id, nombre, dia) VALUES (60, 'La Resurrección', 'Domingo de Resurrección');
 
-INSERT INTO Propietario (id, dni, nombre, apellidos, hermandad_id) VALUES (1, '89088712Y', 'Francisco', 'Vélez de Luna', 52);
-INSERT INTO Propietario (id, dni, nombre, apellidos, hermandad_id) VALUES (2, '21135681S', 'Antonio', 'Martínez López', 40);
+INSERT INTO Propietario (id, dni, nombre, apellidos, hermandad_id) VALUES (1, '89088712Y', 'Francisco', 'Vélez de Luna', 1);
+INSERT INTO Propietario (id, dni, nombre, apellidos, hermandad_id) VALUES (2, '21135681S', 'Antonio', 'Martínez López', 2);
 
 INSERT INTO Sector (id, localizacion, precio) VALUES (1, 'Campana', 299);
 INSERT INTO Sector (id, localizacion, precio) VALUES (2, 'Sierpes', 249);
@@ -71,8 +71,5 @@ INSERT INTO Sector (id, localizacion, precio) VALUES (3, 'Plaza de San Francisco
 INSERT INTO Sector (id, localizacion, precio) VALUES (4, 'Avenida', 199);
 INSERT INTO Sector (id, localizacion, precio) VALUES (5, 'Catedral', 149);
 INSERT INTO Sector (id, localizacion, precio) VALUES (6, 'Plaza Virgen de los Reyes', 289);
-
-
-
 
 ALTER SEQUENCE hibernate_sequence RESTART WITH 1000;
