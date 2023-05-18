e58593bb8c9d92e803e1ec0f74e8625a6155e3b9
Antonio Martinez Lopez <nonomartinezlopez2004@gmail.com>
2023-05-18 13:28:43 +0200

    funcionalidad añadida

--- src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/model/Hermandad.java
+++ src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/model/Hermandad.java
@@ -3,9 +3,7 @@ package com.salesianostriana.dam.proyectoconsejohermandades.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -30,8 +28,7 @@ public class Hermandad {
 	
 	private String nombre, dia;
 	
-	@OneToMany(mappedBy = "hermandad", fetch = FetchType.EAGER,
-			cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "hermandad")
 	@Builder.Default
 	@ToString.Exclude
 	@EqualsAndHashCode.Exclude
