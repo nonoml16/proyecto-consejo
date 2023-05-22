5a1b92acf25174179e86e5e8e62147266e26d99a
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-05-23 01:47:59 +0200

    funcionalidad añadida correctamente

--- src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/controller/HermandadController.java
+++ src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/controller/HermandadController.java
@@ -16,6 +16,7 @@ import com.salesianostriana.dam.proyectoconsejohermandades.model.DiasSemanaSanta
 import com.salesianostriana.dam.proyectoconsejohermandades.model.Hermandad;
 import com.salesianostriana.dam.proyectoconsejohermandades.model.Propietario;
 import com.salesianostriana.dam.proyectoconsejohermandades.service.HermandadService;
+import com.salesianostriana.dam.proyectoconsejohermandades.service.PropietarioService;
 
 @Controller
 @RequestMapping("/admin/hermandad")
@@ -23,6 +24,9 @@ public class HermandadController {
 
 	@Autowired
 	private HermandadService hermandadService;
+	
+	@Autowired
+	private PropietarioService propietarioService;
 	
 	@ModelAttribute("propietario")
 	public Propietario usuario (@AuthenticationPrincipal Propietario propietario) {
@@ -65,13 +69,40 @@ public class HermandadController {
 		
 	}
 	
+	@GetMapping("/hermanos/{id}")
+	public String editarHermanos(@PathVariable("id") Long id, Model model) {
+		Optional<Hermandad> hermandadOpt = hermandadService.findById(id);
+		if(hermandadOpt.isPresent()) {
+			Hermandad hermandad = hermandadOpt.get();
+			model.addAttribute("numHermanos", hermandad.getListaHermanos().size());
+			model.addAttribute("hermandad", hermandad);
+		}
+		return "admin/hermandad/list-hermanos";
+	}
+	
+	@GetMapping("/hermanos/borrar/{id}")
+	public String borrarHermano(@PathVariable("id") Long id, Model model) {
+		Optional<Propietario> propietarioOpt = propietarioService.findById(id);
+		if(propietarioOpt.isPresent()) {
+			Propietario p = propietarioOpt.get();
+			p.setHermandad(null);
+			propietarioService.save(p);
+		}
+		return "redirect:/admin/hermandad/";
+	}
+	
 	@GetMapping("/borrar/{id}")
 	public String borrarHermandad(@PathVariable("id") Long id, Model model) {
 		
-		Optional<Hermandad> hermandad = hermandadService.findById(id);
+		Optional<Hermandad> hermandadOpt = hermandadService.findById(id);
 		
-		if (hermandad.isPresent())
+		if (hermandadOpt.isPresent()) {
+			Hermandad hermandad = hermandadOpt.get();
+			for (Propietario p : hermandad.getListaHermanos()) {
+				p.setHermandad(null);
+			}
 			hermandadService.deleteById(id);
+		}
 
 		return "redirect:/admin/hermandad/";
 		
