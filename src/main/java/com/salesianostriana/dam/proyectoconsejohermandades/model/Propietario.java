902bb240f77815ffd475dcd12e08c7617bb55b4a
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-05-23 02:25:01 +0200

    import.sql ampliado

--- src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/model/Propietario.java
+++ src/main/java/com/salesianostriana/dam/proyectoconsejohermandades/model/Propietario.java
@@ -3,7 +3,6 @@ package com.salesianostriana.dam.proyectoconsejohermandades.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
@@ -37,8 +36,7 @@ public class Propietario extends Usuario{
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_propietario_hermandad"))
 	private Hermandad hermandad;
 	
-	@OneToMany(mappedBy = "propietario", fetch = FetchType.EAGER,
-			cascade = CascadeType.REMOVE, orphanRemoval = true)
+	@OneToMany(mappedBy = "propietario", fetch = FetchType.EAGER)
 	//@Builder.Default
 	@ToString.Exclude
 	@EqualsAndHashCode.Exclude
